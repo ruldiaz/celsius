db3b64b6fea38606bce6f67a93812f288238ae4e
ruldiaz <rulhdiaz@gmail.com>
2024-09-13 21:48:59 -0600

    commit

---
+++ celsius/src/Evaluaciones.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Evaluaciones {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double nota = 0;
+        double mediaEvaluaciones = 0;
+    }
+}
