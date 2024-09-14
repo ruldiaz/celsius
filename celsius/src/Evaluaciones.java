4564707f7dcebc0d5c6860294a6bfd50691baeed
ruldiaz <rulhdiaz@gmail.com>
2024-09-13 21:51:56 -0600

    commit

--- celsius/src/Evaluaciones.java
+++ celsius/src/Evaluaciones.java
@@ -6,5 +6,14 @@ public class Evaluaciones {
 
         double nota = 0;
         double mediaEvaluaciones = 0;
+        double totalEvaluaciones = 0;
+
+        while(nota != -1){
+            System.out.println("Escribe la nota para Matrix");
+            nota = scanner.nextDouble();
+            mediaEvaluaciones += nota;
+            totalEvaluaciones++;
+        }
+        System.out.println("La media de evaluaciones es: " + mediaEvaluaciones/totalEvaluaciones);
     }
 }
